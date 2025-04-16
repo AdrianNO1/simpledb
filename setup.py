b360a731d4e41173be0ab2a7819865c113f80538
Adrian <adrianrosten@gmail.com>
2025-04-16 20:05:59 +0200

    added setup.py for global install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simpledb",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    url="https://github.com/AdrianNO1/simpledb",
+) 
